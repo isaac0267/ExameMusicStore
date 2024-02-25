08fa801ff3d38d2f089956e1620b34d6ec919dd6
Isaac Levy <isaa0267@stud.kea.dk>
2024-02-25 10:12:51 +0100

    Initial commit

---
+++ src/main/java/com/example/kea/ExameMusicStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.kea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExameMusicStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExameMusicStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/examemusicstore/ExameMusicStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examemusicstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExameMusicStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
